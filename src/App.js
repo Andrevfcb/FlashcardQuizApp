eba641206f5af94334ba6593be1153f3ad319b7b
Adrian Majewski <andrevfcb@gmail.com>
2021-01-15 19:40:07 +0100

    added question card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FlashcardList from "./FlashcardList";
+import './app.css'
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASCARDS)
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <FlashcardList flashcards={flashcards} />
   );
 }
+
+const SAMPLE_FLASCARDS = [
+  {
+    id: 1,
+    question: 'What is 2 +2?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+    id: 2,
+    question: 'Question2',
+    answer: '4',
+    options: [
+      'a',
+      'b',
+      'c',
+      'd'
+    ]
+  },
+  {
+    id: 3,
+    question: 'Question3',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  }
+]
 
 export default App;
